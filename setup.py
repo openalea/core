9aad3b3d924779ea049e412672adf0a84cabb416
revesansparole <revesansparole@gmail.com>
2016-07-07 10:40:26 +0200

    use of pkglts

--- setup.py
+++ setup.py
@@ -1,91 +1,66 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""setup file for core package"""
-__revision__ = "$Id$"
+# {# pkglts, pysetup.kwds
+# format setup arguments
 
-import os
-from setuptools import setup
-pj = os.path.join
+from setuptools import setup, find_packages
 
 
-# to get the version
-# execfile("src/core/version.py")
-
-from openalea.deploy.metainfo import read_metainfo
-metadata = read_metainfo('metainfo.ini', verbose=True)
-for key, value in metadata.iteritems():
-    exec("%s = '%s'" % (key, value))
+short_descr = "OpenAlea.Core is able to discover and manage packages and logical components, build and evaluate dataflows and Generate final applications"
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 
-setup(
-    name=name,
-    version=version,
-    description=description,
-    long_description=long_description,
-    author=authors,
-    author_email=authors_email,
-    url=url,
-    license=license,
+def parse_requirements(fname):
+    with open(fname, 'r') as f:
+        txt = f.read()
 
-    namespace_packages=['openalea'],
-    create_namespaces=True,
+    reqs = []
+    for line in txt.splitlines():
+        line = line.strip()
+        if len(line) > 0 and not line.startswith("#"):
+            reqs.append(line)
+
+    return reqs
+
+# find version number in src/openalea/core/version.py
+version = {}
+with open("src/openalea/core/version.py") as fp:
+    exec(fp.read(), version)
+
+
+setup_kwds = dict(
+    name='openalea.core',
+    version=version["__version__"],
+    description=short_descr,
+    long_description=readme + '\n\n' + history,
+    author="openalea, Christophe Pradal, Samuel Dufour-Kowalski, revesansparole, ",
+    author_email="openalea@inria.fr, christophe dot pradal at cirad dot fr, dufourko at cirad dot fr, revesansparole@gmail.com, ",
+    url='https://github.com/openalea/core',
+    license='cecill-c',
     zip_safe=False,
-    include_package_data=True,
 
-    packages=[
-        'openalea.core',
-        'openalea.core.service',
-        'openalea.core',
-        'openalea.core.control',
-        'openalea.core.plugin.builtin',
-        'openalea.core.plugin',
-        'openalea.core.plugin.formatting',
-        'openalea.core.project',
-        'openalea.core.project.formatting',
-        'openalea.core.world',
-        'openalea.core.scene',
-        'openalea.core.system',
-        'openalea.core.interpreter',
-        'openalea.core.algo',
-        'openalea.core.graph',
-        'openalea.core.graph.interface',
-        'openalea.core.formatting',
-        'openalea.core',
-    ],
-
-    package_dir={
-        '': 'src',
-        'openalea.core': pj('src', 'core'),
-        'openalea.core.algo': pj('src', 'core', 'algo'),
-        'openalea.core.control': pj('src', 'core', 'control'),
-        'openalea.core.formatting': pj('src', 'core', 'formatting'),
-        'openalea.core.graph': pj('src', 'core', 'graph'),
-        'openalea.core.graph.interface': pj('src', 'core', 'graph', 'interface'),
-        'openalea.core.interpreter': pj('src', 'core', 'interpreter'),
-        'openalea.core.plugin': pj('src', 'core', 'plugin'),
-        'openalea.core.plugin.builtin': pj('src', 'core', 'plugin', 'builtin'),
-        'openalea.core.project': pj('src', 'core', 'project'),
-        'openalea.core.service': pj('src', 'core', 'service'),
-        'openalea.core.system': pj('src', 'core', 'system'),
-    },
-
-    # Dependencies
-    setup_requires=['openalea.deploy'],
-    install_requires=[],
-    dependency_links=['http://openalea.gforge.inria.fr/pi'],
-
-    share_dirs={'share': 'share'},
-
-    # entry_points
-    entry_points={
-        "wralea": ["openalea.flow control = openalea.core.system", ],
-        "console_scripts": ["alea = openalea.core.alea:main"],
-
-        'openalea.core': [
-            'openalea.core/openalea = openalea.core.plugin.builtin',
-        ],
-    },
-
-
-
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=parse_requirements("requirements.txt"),
+    tests_require=parse_requirements("dvlpt_requirements.txt"),
+    entry_points={},
+    keywords='openalea',
+    test_suite='nose.collector',
 )
+# #}
+# change setup_kwds below before the next pkglts tag
+
+setup_kwds['setup_requires'] = ['openalea.deploy']
+setup_kwds['share_dirs'] = {'share': 'share'}
+setup_kwds['entry_points']["wralea"] = ["openalea.flow control = openalea.core.system", ]
+setup_kwds['entry_points']["console_scripts"] = ["alea = openalea.core.alea:main"]
+setup_kwds['entry_points']['openalea.core'] = [
+            'openalea.core/openalea = openalea.core.plugin.builtin',
+        ]
+
+# do not change things below
+# {# pkglts, pysetup.call
+setup(**setup_kwds)
+# #}
