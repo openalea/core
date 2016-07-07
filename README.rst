a864bc04008ba9a2fe26878fb1d00b4063e413ae
revesansparole <revesansparole@gmail.com>
2016-07-07 10:40:26 +0200

    use of pkglts

---
+++ README.rst
@@ -0,0 +1,10 @@
+========================
+openalea.core
+========================
+
+.. {# pkglts, doc
+
+.. #}
+
+OpenAlea.Core is able to discover and manage packages and logical components, build and evaluate dataflows and Generate final applications
+
--- src/openalea/core/__init__.py
+++ src/openalea/core/__init__.py
@@ -1,17 +1,7 @@
-"""
-See online documentation at
-http://openalea.gforge.inria.fr/doc/sphinx/core/html/contents.html
+# {# pkglts, base
 
-"""
-__license__ = "Cecill-C"
-__revision__ = "$Id$"
+from . import version
 
-from openalea.core.external import *
-from script_library import ScriptLibrary
+__version__ = version.__version__
 
-
-def global_module(module):
-    """ Declare a module accessible everywhere. """
-
-    import __builtin__
-    __builtin__.__dict__[module.__name__] = module
+# #}
