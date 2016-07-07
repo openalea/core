2312b3f7858525c0a4af74d0c96a209f731e5572
revesansparole <revesansparole@gmail.com>
2016-07-07 10:49:07 +0200

    moved source in right directory

--- src/openalea/core/__init__.py
+++ src/openalea/core/__init__.py
@@ -5,3 +5,13 @@ from . import version
 __version__ = version.__version__
 
 # #}
+
+from openalea.core.external import *
+from script_library import ScriptLibrary
+
+
+def global_module(module):
+    """ Declare a module accessible everywhere. """
+
+    import __builtin__
+    __builtin__.__dict__[module.__name__] = module
