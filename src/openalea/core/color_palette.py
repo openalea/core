2fe82a880636bdd5ae299afdfa9eecc80c934215
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2020-01-23 23:53:22 +0100

    apply modernize libmodernize.fixes.fix_raise

--- src/openalea/core/color_palette.py
+++ src/openalea/core/color_palette.py
@@ -27,7 +27,7 @@ def HTMLColorToRGB(colorstring):
     colorstring = colorstring.strip()
     if colorstring[0] == '#': colorstring = colorstring[1:]
     if len(colorstring) != 6:
-        raise ValueError, "input #%s is not in #RRGGBB format" % colorstring
+        raise ValueError("input #%s is not in #RRGGBB format" % colorstring)
     r, g, b = colorstring[:2], colorstring[2:4], colorstring[4:]
     r, g, b = [int(n, 16) for n in (r, g, b)]
     return (r, g, b)
