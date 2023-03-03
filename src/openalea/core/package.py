52109edb71538faebb6720b093b8a785a21acefa
pradal <christophe.pradal@inria.fr>
2023-03-03 08:53:40 -0500

    Generate wralea using latin-1 encoding

--- src/openalea/core/package.py
+++ src/openalea/core/package.py
@@ -724,7 +724,7 @@ class PyPackageWriter(object):
     """ Write a wralea python file """
 
     wralea_template = """
-# -*- coding: utf-8 -*-
+# -*- coding: latin-1 -*-
 # This file has been generated at $TIME
 
 from openalea.core import *
