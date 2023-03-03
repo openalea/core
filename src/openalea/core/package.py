e4bbc87094146f752db968613fdddaecd6e975cd
pradal <christophe.pradal@inria.fr>
2023-03-02 23:07:00 -0500

    Generate utf-8 wralea files

--- src/openalea/core/package.py
+++ src/openalea/core/package.py
@@ -724,6 +724,7 @@ class PyPackageWriter(object):
     """ Write a wralea python file """
 
     wralea_template = """
+# -*- coding: utf-8 -*-
 # This file has been generated at $TIME
 
 from openalea.core import *
