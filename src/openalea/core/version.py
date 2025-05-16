e63e745f631335f8af5c9bfbaeeb66b70302c4f7
pradal <christophe.pradal@cirad.fr>
2025-05-16 23:03:03 +0200

    Update version management

--- src/openalea/core/version.py
+++ src/openalea/core/version.py
@@ -1,12 +1,19 @@
-import importlib.metadata
+#import importlib.metadata
 
-__version__ =  importlib.metadata.version("openalea.core")
+#__version__ =  importlib.metadata.version("openalea.core")
 
-numbers = __version__.split(".")
+#numbers = __version__.split(".")
 
+"""
 try:
     major, minor, post = numbers[0], numbers[1], numbers[2]
     patch = '.'.join(numbers[3:])
 except IndexError:
-    major, minor, post = '2', '4', '2'
+    major, minor, post = '2', '4', '3'
     patch = '0'
+    __version__ = '.'.join([major, minor, post])
+"""
+
+major, minor, post = '2', '4', '3'
+patch = 'a1'
+__version__ = '.'.join([major, minor, post, patch])
