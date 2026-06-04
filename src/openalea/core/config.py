73d4c67e82ba3e46db3be022e03d048e110e6c13
LeticiaNapolitano-34 <leticia.napolitano@etu.umontpellier.fr>
2026-06-04 15:30:11 +0200

    i add 2 news classes Parameter and MyModelUnit and many functions in config.py

--- src/openalea/core/config.py
+++ src/openalea/core/config.py
@@ -19,41 +19,86 @@
 
 __license__ = "Cecill-C"
 
-
+import yaml
+import json
 from openalea.core.singleton import Singleton
 from openalea.core.observer import Observed
+from pathlib import Path
+
+def _load_json(filename: str):
+    """Load configuration from a JSON file."""
+    
+    file = Path(filename)
+    with file.open() as f:
+        data = json.load(f)
+    return data
+
+def _load_yml(filename: str):
+    """Load configuration from a YAML file."""
+
+    file = Path(filename)
+    with file.open() as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
+    return data
+
+def _dump_yml(data, filename: str):
+    """Dump configuration to a YAML file."""
+    
+    file = Path(filename)
+    with file.open("w") as f:
+        yaml.dump(data, f)
+
+def _dump_json(data, filename: str):
+    """Dump configuration to a JSON file."""
+    
+    file = Path(filename)
+    with file.open("w") as f:
+        json.dump(data, f)
+
 
 class Config:
     """Configuration of OpenAlea models
     
     TODO : Documentation to write
     """
-    def __init__(self, model_unit_configs):
+    def __init__(self, model_unit_configs: dict):
         """Initialize the configuration with a list of unit configurations."""
         self.model_unit_configs = model_unit_configs
 
-    @staticmethod
+    def __str__(self):
+        return f"{self.model_unit_configs}"
+
+    #@staticmethod
     def load(self, filename: str):
         """Load configuration from a file.
         
         Dispatch method based on file extension (YAML, JSON, etc.).
         """
-        return Config()
+        
+        extension = filename.split(".")[-1]
+
+        if extension in ("yml", "yaml"):
+            data = _load_yml(filename)
+
+        elif extension == "json":
+            data = _load_json(filename)
+
+        print(data)
+
+        return Config(data)
 
     def dump(self, filename: str):
         """Dump configuration to a file."""
 
-    def _load_yml(self, filename: str):
-        """Load configuration from a YAML file."""
+        extension = filename.split(".")[-1]
 
-    def _dump_yml(self, filename: str):
-        """Dump configuration to a YAML file."""
+        if extension in ("yml", "yaml"):
+            _dump_yml(self.model_unit_configs, filename)
 
-    def _load_json(self, filename: str):
-        """Load configuration from a JSON file."""
+        elif extension == "json":
+            _dump_json(self.model_unit_configs, filename)
 
-    def _dump_json(self, filename: str):
-        """Dump configuration to a JSON file."""
+   
 
     
 #   update()
--- test/test_config.py
+++ test/test_config.py
@@ -3,20 +3,68 @@ import os
 
 from openalea.core.config import Config
 
+import yaml
+import json
+
+class Parameter:
+    def __init__(self, name, value=None, unit=None, param_type=None, description="", uid=None, uri=None):
+        self.name =  name
+        self.value = value
+        self.unit = unit
+        self.param_type = param_type
+        self.description = description
+        self.uid = uid
+        self.uri = uri
+
+    def to_dict(self):
+        return {
+            "name":self.name,
+            "value": self.value,
+            "unit": self.unit,
+            "param_type": self.param_type,
+            "description": self.description,
+            "uid": self.uid,
+            "uri": self.uri
+        }
+
 class MyModelUnit:
-    def __init__(self, name, params={}):
+    def __init__(self, name, parameters: dict):
         self.name = name
+        self.parameters=parameters
+
+    def to_dict(self):
+        return {
+            self.name : self.parameters
+        }
+
+    def __str__(self):
+        return f"name={self.name}, parameters={self.parameters}"
 
     
 def test_config1():
 
-
     unit1 = MyModelUnit('unit1', dict(p1=1, p2='2'))
     unit2 = MyModelUnit('unit2', dict(p1=1, p2='2'))
 
-    config = Config([unit1, unit2])
-    config.dump('toto.yml')
+    p1= Parameter("numerical", 12, "cm", "cm", "mesurer", 234, 1223)
 
+    unit3=MyModelUnit('unit3', p1.to_dict())
+    print(unit3)
+
+    config1 = Config(unit3.to_dict())
+    print(config1)
+    config1.dump("tototo.json")
+    
+    #config1 = Config([unit1, unit2])
+    #config1.dump("toto.json")  
+    config2 = Config.load(unit1, "toto.json")
+    
+    '''
     assert(len(config) == 2)
     assert(len(config['unit1'])==2)
+    '''
+
+test_config1()
+
+
 
