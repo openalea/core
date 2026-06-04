37d32826a10743ac07669bcb0479585d084af271
pradal <christophe.pradal@cirad.fr>
2026-06-04 10:03:32 +0200

    First try

--- src/openalea/core/config.py
+++ src/openalea/core/config.py
@@ -19,6 +19,72 @@
 
 __license__ = "Cecill-C"
 
+
 from openalea.core.singleton import Singleton
 from openalea.core.observer import Observed
 
+class Config:
+    """Configuration of OpenAlea models
+    
+    TODO : Documentation to write
+    """
+    def __init__(self, model_unit_configs):
+        """Initialize the configuration with a list of unit configurations."""
+        self.model_unit_configs = model_unit_configs
+
+    @staticmethod
+    def load(self, filename: str):
+        """Load configuration from a file.
+        
+        Dispatch method based on file extension (YAML, JSON, etc.).
+        """
+        return Config()
+
+    def dump(self, filename: str):
+        """Dump configuration to a file."""
+
+    def _load_yml(self, filename: str):
+        """Load configuration from a YAML file."""
+
+    def _dump_yml(self, filename: str):
+        """Dump configuration to a YAML file."""
+
+    def _load_json(self, filename: str):
+        """Load configuration from a JSON file."""
+
+    def _dump_json(self, filename: str):
+        """Dump configuration to a JSON file."""
+
+    
+#   update()
+#   to_dict()
+#   build_model() --create a model object
+#   generate()
+
+# class ModelUnitConfig
+#   [Parameters]
+#   name
+#   uid
+#   uri
+#   description
+#   validate() --validation des sections
+#   to_dict()
+
+# class Parameter
+#   name
+#   value
+#   unit
+#   type
+#   description
+#   default value
+#   uid
+#   uri
+
+# class ModelConfig
+#   [ModelUnitConfig]
+#   name
+#   def MyModel()
+#      p = params() --recover parameters
+#      c = Config(p)
+#      c.generate('config.yml')
+#   return Model(c)
---
+++ test/test_config.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+import os
+
+from openalea.core.config import Config
+
+class MyModelUnit:
+    def __init__(self, name, params={}):
+        self.name = name
+
+    
+def test_config1():
+
+
+    unit1 = MyModelUnit('unit1', dict(p1=1, p2='2'))
+    unit2 = MyModelUnit('unit2', dict(p1=1, p2='2')
+
+    config = Config([unit1, unit2])
+    config.dump('toto.yml')
+
+    assert(len(config) == 2)
+    assert(len(config['unit1'])==2)
+
+    
