2c35c255681a1df57583e78b4d2395bc5db21350
pradal <christophe.pradal@cirad.fr>
2026-06-04 09:44:34 +0200

    First version for openalea configuration

---
+++ src/openalea/core/config.py
@@ -0,0 +1,24 @@
+# -*- python -*-
+#
+#       OpenAlea.Core
+#
+#       Copyright 2006-2026 CIRAD - INRAE - inria
+#
+#       File author(s): Leticia Napolitano
+#                       Christophe Pradal <christophe.prada@cirad.fr>
+#                       Fabrice Bauget <fabrice.bauget@cirad.fr>
+#
+#       Distributed under the Cecill-C License.
+#       See accompanying file LICENSE.txt or copy at
+#           http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html
+#
+#       OpenAlea WebSite : http://openalea.gforge.inria.fr
+#
+##############################################################################
+"""Configuration of OpenAlea models"""
+
+__license__ = "Cecill-C"
+
+from openalea.core.singleton import Singleton
+from openalea.core.observer import Observed
+
