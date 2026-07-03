6e94dba76959361b382d2ef0db7c8c70ef9529ad
LeticiaNapolitano-34 <leticia.napolitano@etu.umontpellier.fr>
2026-07-03 15:18:48 +0200

    last version of config, new parameters in modelUnit

--- src/openalea/core/config.py
+++ src/openalea/core/config.py
@@ -31,7 +31,10 @@ import io
 from ruamel.yaml import YAML
 
 def _load_json(filename: str):
-    """Load configuration from a JSON file."""
+    """
+    Load a configuration from a JSON file.
+    :param filename: the JSON configuration file.
+    """
     
     file = Path(filename)
     with file.open() as f:
@@ -39,7 +42,10 @@ def _load_json(filename: str):
     return data
 
 def _load_yml(filename: str):
-    """Load configuration from a YAML file."""
+    """
+    Load a configuration from a YAML file.
+    :param filename: the YAML configuration file.
+    """
 
     file = Path(filename)
     with file.open() as f:
@@ -47,20 +53,34 @@ def _load_yml(filename: str):
     return data
 
 def _dump_yml(data, filename: str, sort_keys=False):
-    """Dump configuration to a YAML file."""
+    """
+    Dump configuration to a YAML file.
+    :param data: configuration data to write.
+    :param filename: YAML file path.
+    :param sort_keys: sort keys.
+    """
 
     file = Path(filename)
     with file.open("w") as f:
         yaml.dump(data, f, sort_keys=sort_keys)
 
 def _dump_json(data, filename: str):
-    """Dump configuration to a JSON file."""
+    """
+    Dump configuration to a JSON file.
+    :param data: configuration data to write.
+    :param filename: JSON file path.
+    """
     
     file = Path(filename)
     with file.open("w") as f:
         json.dump(data, f, indent=4)
 
 def to_commented_map(obj):
+    """
+    Convert a Python dictionary into a ruamel.yaml CommentedMap.
+    :param obj: dictionary.
+    """
+
     if isinstance(obj, dict):
         cm = CommentedMap()
         for k, v in obj.items():
@@ -71,8 +91,10 @@ def to_commented_map(obj):
 
 def add_unit_comment(cm, model_unit_configs):
     """
-    Add automatic comments above each ModelUnit section
-    using the optional fields of ModelUnit (description, unit, param_type, uid, uri).
+    Add comments above each ModelUnit section.
+    The comments are generated from optional ModelUnit fields: description, unit, param_type, uid, and uri.
+    :param cm: CommentedMap.
+    :param model_unit_configs: List of ModelUnit objects.
     """
 
     fields = ["description", "unit", "param_type", "uid", "uri"]
@@ -93,11 +115,13 @@ def add_unit_comment(cm, model_unit_configs):
 
 
 def add_comment2(cm, model_unit_configs):
-    '''Add automatic new comments on Parameters (description, unit, param_type, uid and uri).
-    parameters: -cm (CommentedMap) that contains sections and values from parameters.
-                -model_unit_configs: is a list of ModelUnit
-        If the value is None, this function don't add a new comment. If the value is not None, we add the comment.             
-    '''
+    """
+    Add comments to each parameter. Comments are generated from optional Parameter fields: 
+    description, unit, param_type, uid, and uri.
+    If the value is None, this function doesn't add a new comment. If the value is not None, we add the comment.   
+    :param cm: CommentedMap.
+    :param model_unit_configs: List of ModelUnit objects.          
+    """
     
     param_index = {}
     for unit in model_unit_configs:
@@ -121,53 +145,22 @@ def add_comment2(cm, model_unit_configs):
                     before="\n".join(comments)
                 )
 
-'''
-def add_comment(cm, custom_comments=None):
-    
-    if custom_comments is None:
-        custom_comments = {}
-
-    for section_name, section in cm.items():
-        for param_name, param_value in section.items():
-            if param_name in custom_comments:
-                comment = custom_comments[param_name]
-
-                if isinstance(comment, list):
-                    comment = "\n".join(comment)
-
-                section.yaml_set_comment_before_after_key(
-                    param_name,
-                    before=comment
-                )
-
-
-def add_section_comments(cm, section_comments):
-    
-    for section_name in cm.keys():
-        if section_name in section_comments:
-            comment = section_comments[section_name]
-
-            if isinstance(comment, list):
-                comment = "\n".join(comment)
-
-            cm.yaml_set_comment_before_after_key(
-                section_name,
-                before=comment
-            )
-'''
 
 @dataclass 
 class Parameter:
-    '''The dataclass Parameter has two mandatory parameters name and value, the others parameters are optional to the config, if
-    we add them this would be commented, if we don't add them, we don't have comment. We also convert this class in dictionary.'''
+    """
+    Represent a configuration parameter.
+    The dataclass Parameter has two mandatory parameters name and value. The others parameters are optional to the configuration, if
+    we add them this would be commented.
+    The parameter can be converted to dictionary.
+    """
     name: str
-    value: any = None
+    value: any 
     description : any = None
     unit : any = None
     param_type : any = None
     uid : any = None
     uri : any = None
-
 
     def __to_dict__(self):
         return {self.name: self.value}
@@ -177,31 +170,19 @@ class Parameter:
             f"name={self.name}, value={self.value}"
         )
 
-'''
-class ModelUnit(dict):
-    def __init__(self, name, parameters: list):
-    
-        params_dict = {}
-        for p in parameters:
-            params_dict.update(p.__to_dict__())
-
-        super().__init__({name: params_dict})
-        self.name = name
-        self.parameters=parameters
-
-    def __str__(self):
-        return f"name={self.name}, parameters={dict(self)}"
-'''
 
 class ModelUnit(dict):
-    '''ModelUnit is a dictionary with a section name and a list of parameters '''
+    """
+    Represent a configuration sectiion with a name a list of parameters.
+    :param name: Name of the section.
+    :param parameters: List of Parameter objects.
+    :param description: Optional description of the section.
+    """
     def __init__(self, name, parameters: list, description=None, unit=None, param_type=None, uid=None, uri=None):
         super().__init__({name: {p.name: p.value for p in parameters}})
         self.name = name
         self.parameters = parameters
         self.description = description
-        self.unit = unit
-        self.param_type = param_type
         self.uid = uid
         self.uri = uri
 
@@ -210,19 +191,17 @@ class ModelUnit(dict):
 
 
 class Config(dict):
-    """Configuration of OpenAlea models
-    Config is a dictionary that contains a list of model units. With the config can:
-    -Load a new JSON or YAML file.
-    -Dump a JSON or YAML file.
-    -Add new sections.
-    -Add new comments.
-    -The quantity of units.
-    
-    TODO : Documentation to write
+    """
+    Configuration of OpenAlea models.
+    A Config object is a dictionary that contains a list of ModelUnit. A config can load, dump, add new sections, new sections,
+    the quantity of units.
+    :param model_unit_configs: List of ModelUnit objects.
     """
 
     def __init__(self, model_unit_configs: list):
-        """Initialize the configuration with a list of unit configurations."""
+        """
+        Initialize the configuration with a list of unit configurations.
+        """
 
         super().__init__()
         self.model_unit_configs = model_unit_configs
@@ -245,7 +224,11 @@ class Config(dict):
 
 
     def load(self, filename: str):
-        """Load configuration to a file."""
+        """
+        Load a configuration file (YAML or JSON) and return a new Config.
+        :param filename: configuration file.
+        """
+
         extension = filename.split(".")[-1]
 
         if extension in ("yml", "yaml"):
@@ -270,7 +253,10 @@ class Config(dict):
 
 
     def dump(self, filename: str):
-        """Dump configuration to a file."""
+        """
+        Dump the configuration to a YAML or JSON file.
+        :param filename: filename path.
+        """
 
         extension = filename.split(".")[-1]
 
@@ -278,17 +264,30 @@ class Config(dict):
             yaml = YAML()
             cm = to_commented_map(self)
             
-
-            #add_comment(cm, self.params_comments)
             add_comment2(cm, self.model_unit_configs)
             add_unit_comment(cm, self.model_unit_configs)
-            #add_section_comments(cm, self.section_comments)
-
 
             with open(filename, "w") as f:
                 yaml.dump(cm, f)
 
         elif extension == "json":
             _dump_json(dict(self), filename)
+
+    def get_sections(self):
+        """
+        Return the list of section names in the configuration.
+        """
+
+        return list(self.keys())
+
+    def get_parameters(self, section_name):
+        """
+        Return the list of parameter names of a section.
+        :param section_name: Name of the section.
+        """
+
+        if section_name not in self:
+            raise KeyError(f"Section '{section_name}' doesn't exist.")
+        return list(self[section_name].keys())
 
 
